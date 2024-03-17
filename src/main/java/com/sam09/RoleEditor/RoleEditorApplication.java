16aa3fe4ab4073367730d1094d302f712b96bbaa
Soumyabrata Sen <soumyabratasen@hotmail.com>
2024-03-17 19:48:49 +0530

    Initial Commit

---
+++ src/main/java/com/sam09/RoleEditor/RoleEditorApplication.java
@@ -0,0 +1,13 @@
+package com.sam09.RoleEditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleEditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sam09/RoleEditor/RoleEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam09.RoleEditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
