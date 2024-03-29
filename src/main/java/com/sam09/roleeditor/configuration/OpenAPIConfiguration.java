a3bb5975af3c5b06822baaba83d5fa3a78db8106
Soumyabrata Sen <soumyabratasen@hotmail.com>
2024-03-29 20:37:32 +0530

    Details added to swagger doc

---
+++ src/main/java/com/sam09/roleeditor/configuration/OpenAPIConfiguration.java
@@ -0,0 +1,34 @@
+package com.sam09.roleeditor.configuration;
+
+import com.sam09.roleeditor.utils.ApplicationConstants;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = ApplicationConstants.TITLE,
+                description = ApplicationConstants.DESCRIPTION,
+                termsOfService = ApplicationConstants.TERMS,
+                version = ApplicationConstants.VERSION,
+                contact = @Contact(
+                        name = "V3nom09_Sentinel",
+                        email = "sen.soumyabrata09@yahoo.com"
+                ),
+                license = @License(
+                        name = "MIT License",
+                        url = "https://choosealicense.com/licenses/mit/"
+                )
+        ),
+        servers = {
+                @Server(
+                        description = "dev-server",
+                        url = "http:localhost:8081/api/v1"
+                )
+        }
+)
+public class OpenAPIConfiguration {
+
+}
